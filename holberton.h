4dd60de8a956a6c25da1ee710f2d6d8d6712119f
JosePabloBonilla <2750@holbertonschool.com>
2021-03-13 17:18:33 +0000

    adding header file

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif
