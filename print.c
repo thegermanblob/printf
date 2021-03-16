670f2493a15e736e47d6fc2eae827d99f85f0199
Thegermanblob <thegermanblob@gmail.com>
2021-03-16 18:29:06 +0000

    edge case % only

--- print.c
+++ print.c
@@ -21,7 +21,11 @@ int _printf(const char *format, ...)
 			len++;
 			i++;
 		}
-		if (format[i] == '%')
+		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 'd'
+					|| format[i + 1] == 'i' || format[i + 1] == 's' || format[i + 1] == 'b'
+					|| format[i + 1] == 'p' || format[i + 1] == 'o' || format[i + 1] == 'x'
+					|| format[i + 1] == 'X' || format[i + 1] == '%' || format[i + 1] == 'u'
+					|| format[i + 1] == 'r' || format[i + 1] == 'S' || format[i + 1] == 'R'))
 			i++;
 		len += switcher(arg, format[i]);
 		len += switcher2(arg, format[i]);
